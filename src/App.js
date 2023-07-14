e195a66431d0a63583a4824a40cebdc7776dc53f
Abideen <misterjrshittu@gmail.com>
2023-07-14 13:27:44 +0100

    revise useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 
 import './App.css';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
+
 
 const App = () => {
-
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     alert("You've changed the counter to " + counter)
-  }, [counter])
+  }, [counter]);
 
   return (
     <div className="App">
-      <button onClick={() => setCounter( (prevCount) => prevCount - 1)}>-</button>   
+      <button onClick={ () => setCounter( (prevCount) => prevCount - 1 )}>-</button>
       <p>{counter}</p>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>+</button>
+      <button onClick={ () => setCounter ((prevCount) => prevCount + 1)}>+</button>
     </div>
   );
 }

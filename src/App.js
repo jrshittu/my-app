87c569280bef7d7106243648aec26810f6cd2ec5
Abideen <misterjrshittu@gmail.com>
2023-07-15 11:59:29 +0100

    fix missing semi-colon

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=a5afc377'
 const App = () => {
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s={title}`)
+    const response = await fetch(`${API_URL}&s={title}`);
     const data = await response.json();
     console.log(data.Search);
   }
